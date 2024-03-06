bc39ae081403e928817a3906955f0245044c56a2
Johan <becerrajohan6@gmail.com>
2024-03-05 22:19:34 -0500

    vercel

--- db/db.js
+++ db/db.js
@@ -1,17 +1,29 @@
 var mysql = require("mysql");
-
+// local con xampp
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "fundo_becerra",
+// });
+/*
+Anfitrión: sql6.freesqldatabase.com
+Nombre de la base de datos: sql6689014
+Usuario de la base de datos: sql6689014
+Contraseña de la base de datos: u9DTru6xlL
+Número de puerto: 3306
+ */
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "fundo_becerra",
+  host: "sql6.freesqldatabase.com",
+  user: "sql6689014",
+  password: "u9DTru6xlL",
+  database: "sql6689014",
 });
 
 con.connect(function (err) {
   if (err) throw err;
   console.log("Conectado!");
   console.log();
-}
-);
+});
 
 module.exports = con; // Exportamos la conexión para poder utilizarla en otros archivos
